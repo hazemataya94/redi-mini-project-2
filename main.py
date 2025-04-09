5d0e8c733aacb66a38b6bf5ed925301bd8e4703f
Hazem Ataya <hazem.ataya94@gmail.com>
2025-04-09 19:44:21 +0200

    add main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import json
 from file_service import FileService
 
+csv_file_path = "template.csv"
+
 if __name__ == "__main__":
-    file_service = FileService()
     
-    data_dictionary = file_service.read_csv_file("template.csv")
-    print(data_dictionary)
+    file_service = FileService()
+    data_dictionary = file_service.read_csv_file(csv_file_path)
+    print(json.dumps(data_dictionary, indent=4))
