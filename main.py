6d6bc1fb8825372ecdeb3452cc39cfba4dc10a43
Hazem Ataya <hazem.ataya94@gmail.com>
2025-04-23 19:58:36 +0200

    main py

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from database_training.sqlite_connect import RediSQLiteConnection
 import logging
+logging.basicConfig(level=logging.DEBUG)
+
 if __name__ == "__main__":
     db_path = "redi.db"
-    logging.basicConfig(level=logging.DEBUG)
+
     logging.info("Connecting to the database 'redi.db'")
     redi_db = RediSQLiteConnection(db_path)
-    redi_db.create_table()
+    
+    ### Run Your Code Here
+    ###
+    ###
+    ###
+    ###
+    ###
+    ###
+    ###
+    ###
+
     logging.info("Closing the database connection")
     redi_db.close()
